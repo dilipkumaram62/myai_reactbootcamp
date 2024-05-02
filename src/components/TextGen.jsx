34704e33fc8d1c18140adf1f56990740bd2ff0e3
Dilip Kumar <dilipkumaram62@gmail.com>
2024-05-02 16:17:21 +0530

    add loging logo

--- src/components/TextGen.jsx
+++ src/components/TextGen.jsx
@@ -1,56 +1,76 @@
 import React, { useState } from 'react'
-import{ GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY); // initialization
 // function for text generation
 async function GenText(promptprovided) {
-    // For text-only input, use the gemini-pro model
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  
-    const prompt = promptprovided;  // hum prompt le rhe h 
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-    return text;
-  }
-  
-  GenText();
+  // setLoading(true);
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = promptprovided;  // hum prompt le rhe h 
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  // setLoading(false);
+  return text;
+}
+
+GenText();
 
 // taking input 
 const TextGen = () => {
-    const[prompt,setprompt] = useState("");
-    const[response,setResponse] = useState("");
-    function handleChange(e){
-        setprompt(e.target.value);
-    }
+  const [prompt, setprompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  function handleChange(e) {
+    setprompt(e.target.value);
+  }
 
-    async function handleSubmit(){
-        const generatedResponse = await GenText(prompt);
-        setResponse(generatedResponse);
-        console.log(response);
+  async function handleSubmit() {
+    // setLoading(true);
+    const generatedResponse = await GenText(prompt);
+    setResponse(generatedResponse);
+    console.log(response);
 
-    }
+  }
   return (
-    <div className='max-w-screen-xl mx-auto'>
-        <h1 className='text-center text-4xl text-blue-900'>
-            MY AI : Text Generation
-        </h1>
-        <div className='my-10 mx-auto max-w-screen-lg'>
-        <label className='block my-4'htmlFor="Enter your prompt">Enter your prompt</label>
-        <textarea  name="promptText" id="promptText" cols="30" rows="5" onChange={(e)=>handleChange(e)}  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-         placeholder='Hi!'></textarea>
-        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-1
-         focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 my-4" onClick={handleSubmit}>Generate{""}</button>
-        </div>
-        <section class="max-w-4xl mx-auto p-4"><h4 class="text-xl font-bold py-2"></h4><article class="text-lg"><p class="text-lg py-2"></p></article></section>
-        
-        <div className='my-4 max-w-screen-xl'>
-            <p>{response}</p>
+    <section className="max-w-4xl mx-auto bg-slate-200 px-4">
+      <h1 className='text-center text-4xl text-blue-900'>
+        MY AI : Text Generation
+      </h1>
+      <div className='my-10 mx-auto max-w-screen-lg'>
+        <label className='block my-4' htmlFor="Enter your prompt">Enter your prompt</label>
+        <textarea name="promptText" id="promptText" cols="30" rows="5" onChange={(e) => handleChange(e)} className="block p-2.5 w-full text-sm bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+          placeholder='Hi!'></textarea>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-1
+         focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 my-4" onClick={handleSubmit}>
+          Generate{""}</button>
+      </div>
+      <section className="max-w-4xl mx-auto p-4"><h4 className="text-xl font-bold py-2"></h4><article className="text-lg"><p className="text-lg py-2"></p></article></section>
 
-        </div>
-    </div>
-  )
+
+
+<div className='my-4 max-w-screen-xl'>
+      
+  {isLoading? (
+  <div role="status">
+    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span className="sr-only">Loading...</span>
+    </div> 
+    ):(
+      <p>{response}</p>
+    )}
+  
+
+</div>
+    </section>
+    
+  );
 }
 
 export default TextGen;
